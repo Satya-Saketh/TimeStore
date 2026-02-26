615ddbb1995a56436b369ca771d5606421a897e7
Satya-Saketh <satyasaketh2003@gmail.com>
2026-02-26 10:26:13 +0530

    db url removed

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,18 @@
 import mongoose from "mongoose";
-
-export const connectDB = async()=>{
+import dotenv from 'dotenv'
+export const connectDB = async () => {
 
     try {
 
-        await mongoose.connect('mongodb+srv://satyasaketh2003_db_user:satyasaketh2003_db_user@cluster0.rebdfos.mongodb.net/?appName=Cluster0');
+        const DB_URL = process.env.DB_URL
+
+        await mongoose.connect(DB_URL);
         console.log("Connected to DB")
-        
+
     } catch (error) {
 
         console.log("Error While Connecting to DB", error);
-        
+
     }
 
 }
